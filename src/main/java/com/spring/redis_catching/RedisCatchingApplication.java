671b521cc7b7bc04682d82ec6f46fc62ce8166a1
bungearntun <love2554f@hotmail.com>
2025-03-04 10:02:27 +0700

    initialize application

---
+++ src/main/java/com/spring/redis_catching/RedisCatchingApplication.java
@@ -0,0 +1,13 @@
+package com.spring.redis_catching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisCatchingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisCatchingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/redis_catching/RedisCatchingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.redis_catching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisCatchingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

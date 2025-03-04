8aa2f316da5f39ab0bccccf48b209350652c652d
bungearntun <love2554f@hotmail.com>
2025-03-04 11:14:29 +0700

    changed : redis url

--- src/test/java/com/spring/redis_catching/config/RedissonConfig.java
+++ src/test/java/com/spring/redis_catching/config/RedissonConfig.java
@@ -15,7 +15,7 @@ public class RedissonConfig {
         if (Objects.isNull(this.redissonClient)) {
             Config config = new Config();
             config.useSingleServer()
-                    .setAddress("redis://127.0.0.1:6379");
+                    .setAddress("redis://localhost:6379");
             redissonClient = Redisson.create(config);
         }
         return redissonClient;
